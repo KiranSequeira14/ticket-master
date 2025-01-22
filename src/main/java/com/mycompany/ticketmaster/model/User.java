8ce04d0f9926a730ca1e4bfd8d0f352729f197b3
kiran <kiransequeira884@gmail.com>
2025-01-22 23:21:27 +0530

    Authentication - 1

--- src/main/java/com/mycompany/ticketmaster/model/User.java
+++ src/main/java/com/mycompany/ticketmaster/model/User.java
@@ -2,20 +2,32 @@ package com.mycompany.ticketmaster.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private String name;
-    private String emailId;
-
+    private String email;
+    private String password;
+    private String role;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonManagedReference
     private List<Ticket> tickets;
@@ -23,4 +35,17 @@ public class User {
     public void addTicket(Ticket ticket) {
         tickets.add(ticket);
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(this.role));
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
 }
--- src/main/java/com/mycompany/ticketmaster/repository/UserRepository.java
+++ src/main/java/com/mycompany/ticketmaster/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.mycompany.ticketmaster.repository;
 import com.mycompany.ticketmaster.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByEmail(String emailId);
 }
